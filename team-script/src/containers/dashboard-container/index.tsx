28147652c2ee034e2379944c5be562a6873c5ff8
alimoqimi97 <41248132+alimoqimi97@users.noreply.github.com>
2024-03-07 13:44:35 +0330

    feature/implement-editor
    - add dashboard container

---
+++ team-script/src/containers/dashboard-container/index.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { FC } from "react";
+
+
+const DashboardContainer:FC = () => {
+    return (
+        <section>
+            
+        </section>
+    )
+}
+
+export default DashboardContainer
