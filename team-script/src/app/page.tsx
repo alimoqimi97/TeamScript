d2c538af76c999317174f75ff0825f30389c1ae9
alimoqimi97 <41248132+alimoqimi97@users.noreply.github.com>
2024-03-07 14:11:07 +0330

    feature/implement-editor
    - implement editor onmount handler

--- team-script/src/app/page.tsx
+++ team-script/src/app/page.tsx
@@ -2,8 +2,8 @@ import DashboardContainer from "@/containers/dashboard-container";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <DashboardContainer  />
+    <main className="">
+      <DashboardContainer />
     </main>
   );
 }
