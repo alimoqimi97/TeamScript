52c69dae215d1b86f94de99f7d39b0ea5c6f00e7
alimoqimi97 <41248132+alimoqimi97@users.noreply.github.com>
2024-05-02 15:06:39 +0330

    Feat: add video chat
    -Implement server

--- server.js
+++ server.js
@@ -7,19 +7,36 @@ const next = require("next");
 const { Server } = require("socket.io");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
+const HOSTNAME = "localhost";
+const PORT = process.env.port || 5000;
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port });
+const app = next({ dev, hostname: HOSTNAME, port: PORT });
 const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
   const httpServer = createServer(handler);
 
-  const io = new Server(httpServer);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
 
   io.on("connection", (socket) => {
-    // ...
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", () => {
+      socket.broadcast.emit("callended");
+    });
+
+    socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+      io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+    });
+
+    socket.on('answercall', (data) => {
+      io.to(data.to).emit('callaccepted', data.signal)
+    })
   });
 
   httpServer
@@ -27,7 +44,7 @@ app.prepare().then(() => {
       console.error(err);
       process.exit(1);
     })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`);
+    .listen(PORT, () => {
+      console.log(`> Ready on http://${HOSTNAME}:${PORT}`);
     });
 });
